df1cbc10e1f097fbe8a73d8026259842b1aa3b59
monavixx <dperelygin0@gmail.com>
2026-07-28 15:07:32 +0300

    toggle touchpad

--- hosts/laptop/hyprland.lua
+++ hosts/laptop/hyprland.lua
@@ -51,3 +51,16 @@ hl.bind("switch:Lid Switch", hl.dsp.exec_cmd("hyprlock"));
 
 hl.bind("XF86MonBrightnessUp",  hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"),                  { locked = true, repeating = true })
 hl.bind("XF86MonBrightnessDown",hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"),                  { locked = true, repeating = true })
+
+
+local touchpad_name = "htix5288:00-093a:0255-touchpad"
+local touchpad_enabled = true
+hl.bind("SHIFT + META + f23", function()
+    touchpad_enabled = not touchpad_enabled
+    hl.device({
+        name = touchpad_name,
+        enabled = touchpad_enabled
+    })
+    -- Optional: Send a desktop notification to confirm the toggle state
+    hl.dispatch(hl.dsp.exec_cmd("notify-send -t 1000 'Touchpad' '" .. (touchpad_enabled and "Enabled" or "Disabled") .. "'"))
+end)
