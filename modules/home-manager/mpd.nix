b6f0e3d235ac835c77c6ad5a59d6ef8cd457d29a
monavixx <dperelygin0@gmail.com>
2026-08-04 12:42:19 +0300

    mpd fix

--- modules/home-manager/mpd.nix
+++ modules/home-manager/mpd.nix
@@ -4,6 +4,15 @@
     enable = true;
     network.startWhenNeeded = true;
     musicDirectory = "${config.home.homeDirectory}/Music";
+    extraConfig = ''
+      audio_output {
+        type "pipewire"
+        name "PipeWire"
+      }
+    '';
   };
-  services.mpdris2.enable = true;
+  services.mpdris2 = {
+    enable = true;
+    multimediaKeys = true;
+  };
 }
